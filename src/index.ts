43867c491e20e6de8d2c1a8749c455017dd0b153
Ester Oliveira <oliveiraester466@gmail.com>
2023-09-26 21:43:01 -0300

    Aula do dia 26/09/2023

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,56 @@
-import { Categoria } from "@modules/catalogo/domain/categoria/categoria.entity";
-import { PrismaClient } from "@prisma/client";
-import { DomainException } from "@shared/domain/domain.exception";
+import { Categoria } from '@modules/catalogo/domain/categoria/categoria.entity';
+import { CategoriaPrismaRepository } from '@modules/catalogo/infra/database/categoria.prisma.repository';
+import { PrismaClient } from '@prisma/client';
+import { DomainException } from '@shared/domain/domain.exception';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    log: ['query', 'info'],
+    errorFormat: 'pretty'
+});
 
 async function main() {
-
-    let categoria: Categoria;
-    categoria = Categoria.criar({nome:'mesa'});
-
-    /*await prisma.categoria.create({
-        data: {
-            id: categoria.id,
-            nome: categoria.nome
+   
+    prisma.$connect().then(
+        async () => {
+            console.log('Postgres Conectado');
         }
-    });*/
+    );
 
-    const categoriarecuperada = await prisma.categoria.update({
-        where: {id: "2eca23b4-d03b-48c2-a0ee-91f19a9c198f"},
-        data: {nome: 'banho'},
-    })
+    const categoriaRepo = new CategoriaPrismaRepository(prisma);
 
-    const ListaCategorias = await prisma.categoria.findMany();
-    console.log(ListaCategorias);
+    //const categoriaRecuperada = await categoriaRepo.recuperarPorUuid("c2666bdb-c055-40bb-951b-32f899f41e30");
+
+    //console.log(categoriaRecuperada);
+
+    //const categoria: Categoria = Categoria.criar({
+    //    nome:'Banho'
+    //})    
+
+    //const categoriaInserida = await categoriaRepo.inserir(categoria);
+
+    //console.log(categoriaInserida);
+
+    //const categorias = await categoriaRepo.recuperarTodos();
+
+    //console.log(categorias);
+
+    const categoriaAtualizada = await categoriaRepo.atualizar();
 
 }
 
 main()
-
     .then(async () => {
         await prisma.$disconnect()
     })
     .catch(async (error) => {
-        if (error instanceof DomainException) {
-            console.log('Execeção de Domínio');
-            console.log(error.message);
-        }
-        else {
-            console.log('Outras Execeções');
-            console.log(error.message);
-        }
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+       if (error instanceof DomainException) {
+           console.log('Execeção de Dóminio');
+           console.log(error.message);
+       }
+       else {
+           console.log('Outras Exceções');
+           console.log(error.message);
+       }
+       await prisma.$disconnect()
+       process.exit(1)
+   })
---
+++ src/modules/catalogo/domain/categoria/categoria.repository.interface.ts
@@ -0,0 +1,6 @@
+import { IRepository } from "@shared/domain/repository.interface";
+
+interface ICategoriaRepository<T> extends IRepository<T> {
+}
+
+export { ICategoriaRepository }
