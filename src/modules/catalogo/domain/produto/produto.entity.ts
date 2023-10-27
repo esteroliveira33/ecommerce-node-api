149990c891dfbab6e17949edc881e55d7acfe544
Ester Oliveira <oliveiraester466@gmail.com>
2023-10-26 21:08:10 -0300

    Aula do dia 26/10/2023

--- src/modules/catalogo/domain/produto/produto.entity.ts
+++ src/modules/catalogo/domain/produto/produto.entity.ts
@@ -4,6 +4,7 @@ import { Categoria } from "../categoria/categoria.entity";
 import { ProdutoExceptions } from "./produto.exception";
 import { CriarProdutoProps, IProduto, RecuperarProdutoProps, StatusProduto } from "./produto.types";
 import { NomeCategoriaNuloOuIndefinido } from "../categoria/categoria.exception";
+import { RecuperarCategoriaProps } from "../categoria/categoria.types";
 
 class Produto extends Entity<IProduto> implements IProduto {
 
@@ -146,7 +147,7 @@ class Produto extends Entity<IProduto> implements IProduto {
         this.nome = produto.nome;
         this.descricao = produto.descricao;
         this.valor = produto.valor;
-        this.categorias = produto.categorias;
+        this.categorias = produto.categorias.map((categoria) => { return Categoria.recuperar(categoria as RecuperarCategoriaProps)});
         this.dataCriacao = produto.dataCriacao;
         this.dataAtualizacao = produto.dataAtualizacao;
         this.dataExclusao = produto.dataExclusao;
